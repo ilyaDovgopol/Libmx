0fa7c4a11903fd056eb28eff8e7b9cbf1d464d1b
Ilya Dovgopol <idovgopol@e2r9p6.unit.ua>
2019-11-09 15:06:12 +0200

    libmx

--- src/mx_str_reverse.c
+++ src/mx_str_reverse.c
@@ -7,7 +7,7 @@ void mx_str_reverse(char *s) {
 	int len = mx_strlen(s);
 
 	for (int i = 0; i < len / 2; i++) {
-		mx_swap_char(&s[i], &s[len - i - 1]);
+		mx_swap_char(&s[i], &s[len - 1 - i]);
 	}
 }
 
