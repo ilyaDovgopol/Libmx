18956a5a0a73dc6562a0380779a31e14a1f41406
Ilya Dovgopol <idovgopol@e2r9p6.unit.ua>
2019-11-16 15:22:12 +0200

    libmx

--- src/mx_binary_search.c
+++ src/mx_binary_search.c
@@ -1,7 +1,7 @@
 #include "libmx.h"
 
 int mx_binary_search(char **arr, int size, const char *s, int *count) {
-	if (!arr || !(*arr) || !s) {
+	if (!arr || !(*arr) || !s || !(*s)) {
 		return -1;
 	}
 	int left = 0;
