16fd6138b7e6f7c926f9601680ee59d73db81e6a
Ilya Dovgopol <idovgopol@e2r14p2.unit.ua>
2019-12-20 16:08:41 +0200

    libmx

--- src/mx_strcat.c
+++ src/mx_strcat.c
@@ -1,15 +1,7 @@
 #include "libmx.h"
 
 char *mx_strcat(char *restrict s1, const char *restrict s2) {
-    int i = mx_strlen(s1);
-    int j = 0;
-
-    if (s2) {
-        while ((s1[i] = s2[j])) {
-            i++;
-            j++;
-        }
-    }
+    mx_strcpy(s1 + mx_strlen(s1), s2);
     return s1;
 }
 
