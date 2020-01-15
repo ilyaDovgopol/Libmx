ea911828ad8728f766944e1187594667e30cdb00
Ilya Dovgopol <idovgopol@e2r13p2.unit.ua>
2020-01-15 18:48:48 +0200

    libmx

--- src/mx_print_strarr.c
+++ src/mx_print_strarr.c
@@ -4,7 +4,7 @@ void mx_print_strarr(char **arr, const char *delim) {
     if (!arr || !(**arr) || !delim) {
         return;
     }
-    mx_printstr(arr[0]);
+    mx_printstr(*arr);
     for (int i = 1; arr[i] != NULL; i++) {
         mx_printstr(delim);
         mx_printstr(arr[i]);
