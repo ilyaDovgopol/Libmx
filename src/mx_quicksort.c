e0fd9496c3b07f2a5f568d85df669bfcc740d1d8
Ilya Dovgopol <idovgopol@e2r13p2.unit.ua>
2020-01-15 13:46:21 +0200

    libmx

--- src/mx_quicksort.c
+++ src/mx_quicksort.c
@@ -1,34 +1,38 @@
 #include "libmx.h"
 
+static void quicksort(char **arr, int *i, char *pivot, int *j);
 static void swap(char **s1, char **s2, int *count);
 
 int mx_quicksort(char **arr, int left, int right) {
     int i = left;
     int j = right;
-    char *pivot;
     int count = 0;
 
     if (!arr)
         return -1;
     if (left >= right)
         return 0;
-    pivot = arr[(left + right) / 2];
     while (i <= j) {
-        while (mx_strlen(arr[i]) - mx_strlen(pivot) < 0)
-            i++;
-        while (mx_strlen(arr[j]) - mx_strlen(pivot) > 0)
-            j--;
-        if (i <= j) {
+        quicksort(arr, &i, arr[(left + right) / 2], &j);
+        if (i <= j)
             swap(&arr[i], &arr[j], &count);
-            i++;
-            j--;
-        }
+        i++;
+        j--;
     }
     if (left < j)
         count += mx_quicksort(arr, left, j);
     if (right > i)
         count += mx_quicksort(arr, i, right);
     return count;
+}
+
+static void quicksort(char **arr, int *i, char *pivot, int *j) {
+    while (mx_strlen(arr[*i]) - mx_strlen(pivot) < 0) {
+        (*i)++;
+    }
+    while (mx_strlen(arr[*j]) - mx_strlen(pivot) > 0) {
+        (*j)--;
+    }
 }
 
 static void swap(char **s1, char **s2, int *count) {
